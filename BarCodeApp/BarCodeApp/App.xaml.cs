bf68440df1a0da0ecc2a2c4baf971a3df82fe14f
AbelTomas16 <abeljosetoms115@gmail.com>
2021-09-27 23:03:10 +0100

    Add project files.

---
+++ BarCodeApp/BarCodeApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF.Material.Forms;
+using XF.Material.Forms.UI;
+
+namespace BarCodeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Material.Init(this);
+            MainPage = new MaterialNavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
