e4c50f27f40164cdb74aaceac2b6da8e64580b28
AbelTomas16 <abeljosetoms115@gmail.com>
2021-10-03 02:04:08 +0100

    refactor:dando permissão no essential

--- BarCodeApp/BarCodeApp.Android/MainActivity.cs
+++ BarCodeApp/BarCodeApp.Android/MainActivity.cs
@@ -20,7 +20,7 @@ namespace BarCodeApp.Droid
             base.OnCreate(savedInstanceState);
             ZXing.Net.Mobile.Forms.Android.Platform.Init();
             XF.Material.Droid.Material.Init(this, savedInstanceState);
-            // Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
@@ -28,9 +28,9 @@ namespace BarCodeApp.Droid
         [Obsolete]
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
-           // Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             global::ZXing.Net.Mobile.Forms.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            //base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
