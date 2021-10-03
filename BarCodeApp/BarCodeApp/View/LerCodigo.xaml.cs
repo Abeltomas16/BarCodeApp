900aacdcd56a633ea924ca53c465d9d728ced9d8
AbelTomas16 <abeljosetoms115@gmail.com>
2021-10-03 01:44:20 +0100

    refactor: adicionadas melhorias na interface

--- BarCodeApp/BarCodeApp/View/LerCodigo.xaml.cs
+++ BarCodeApp/BarCodeApp/View/LerCodigo.xaml.cs
@@ -16,14 +16,13 @@ namespace BarCodeApp.View
         {
             Device.BeginInvokeOnMainThread(async () =>
             {
-                scannv.IsTorchOn = true;
                 scannv.IsScanning = false;
                 await DisplayAlert("Verificação", "Status: liberado \nNome:Abel\nChave: ##\nKeyCode:" + result.Text, null, "Ok");
                 scannv.IsScanning = true;
-                scannv.IsTorchOn = false;
             });
         }
         private void ContentPage_Appearing(object sender, EventArgs e) => scannv.IsVisible = true;
         private void ContentPage_Disappearing(object sender, EventArgs e) => scannv.IsVisible = false;
+        private void flash_Clicked(object sender, EventArgs e)=> scannv.IsTorchOn = !scannv.IsTorchOn;         
     }
 }
