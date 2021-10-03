aba07fc6be1bf1d2cf94184595d46645dcc59dec
AbelTomas16 <abeljosetoms115@gmail.com>
2021-10-03 01:01:45 +0100

    refactor: add menu tabbed e resolvido a tela preta

--- BarCodeApp/BarCodeApp/MainPage.xaml.cs
+++ BarCodeApp/BarCodeApp/MainPage.xaml.cs
@@ -1,36 +1,17 @@
-﻿using BarCodeApp.View;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using Xamarin.Forms;
-using XF.Material.Forms.UI;
 
 namespace BarCodeApp
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
             InitializeComponent();
-
-            btGerar.Clicked += btGerar_Clicked;
-            btLer.Clicked += btGerar_Clicked;
-        }
-
-        private async void btGerar_Clicked(object sender, EventArgs e)
-        {
-            Button bt = (sender as Button);
-            if (bt.Text.ToUpper().Equals("LER"))
-                await Navigation.PushAsync(new LerCodigo());
-            else if (bt.Text.ToUpper().Equals("GERAR"))
-                await Navigation.PushAsync(new GerarCodigo());
-        }
         }
     }
+}
 
--- BarCodeApp/BarCodeApp/View/LerCodigo.xaml.cs
+++ BarCodeApp/BarCodeApp/View/LerCodigo.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -16,15 +12,18 @@ namespace BarCodeApp.View
         {
             InitializeComponent();
         }
-
         private void ZXingScannerView_OnScanResult(ZXing.Result result)
         {
             Device.BeginInvokeOnMainThread(async () =>
             {
+                scannv.IsTorchOn = true;
                 scannv.IsScanning = false;
-                await DisplayAlert("Verificação", "Status: liberado \nNome:Abel\nChave: ##\nKeyCode:"+result.Text, null, "Ok");
+                await DisplayAlert("Verificação", "Status: liberado \nNome:Abel\nChave: ##\nKeyCode:" + result.Text, null, "Ok");
                 scannv.IsScanning = true;
+                scannv.IsTorchOn = false;
             });
         }
+        private void ContentPage_Appearing(object sender, EventArgs e) => scannv.IsVisible = true;
+        private void ContentPage_Disappearing(object sender, EventArgs e) => scannv.IsVisible = false;
     }
 }
